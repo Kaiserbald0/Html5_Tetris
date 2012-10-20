1fd05bc3e97928af137a5b9d58d5d894591f2f1d
Kaiserbaldo <kaiserbaldo@gmail.com>
2012-10-20 17:13:39 +0100

    New functionality:   -Pause   -Redefine key   Refactor:    -Some translation from Italian to English    Other:    -Added mootools library to interact with the dom

--- js/Stage.js
+++ js/Stage.js
@@ -1,6 +1,6 @@
 /*
-   @author Marco Baldini
-   @license GNU General Public License, version 2 (GPLv2)
+    @author Kaiserbaldo - kaiserbaldo@gmail.com
+    @license GNU General Public License, version 2 (GPLv2)
  */
 
 function Stage(images) {
@@ -11,9 +11,12 @@ function Stage(images) {
     this.righeToNextLevel = 10;
     this.hold = 0;
     this.switched = 0;
-    
-    //i pixel
-    this.pixel = new Array();
+    this.paused = 0;
+
+    this.LoadKeyConfiguration();
+
+    //pixels
+    this.pixel = [];
     this.pixel[0] = images.nero;
     this.pixel[1] = images.blu;
     this.pixel[2] = images.rosso;
@@ -27,51 +30,51 @@ function Stage(images) {
     this.whitePixel = images.white;
 
 
-    this.next = new Array();
+    this.next = [];
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextLO;
     nextP[1] = images.nextLO1;
     nextP[2] = images.nextLO2;
 
     this.next[6] = nextP;
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextZ;
     nextP[1] = images.nextZ1;
     nextP[2] = images.nextZ2;
 
     this.next[4] = nextP;
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextL;
     nextP[1] = images.nextL1;
     nextP[2] = images.nextL2;
 
     this.next[5] = nextP;                
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextI;
     nextP[1] = images.nextI1;
     nextP[2] = images.nextI2;
 
     this.next[2] = nextP;
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextO;
     nextP[1] = images.nextO1;
     nextP[2] = images.nextO2;
 
     this.next[1] = nextP;
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextT;
     nextP[1] = images.nextT1;
     nextP[2] = images.nextT2;
 
     this.next[7] = nextP;
 
-    nextP = new Array();
+    nextP = [];
     nextP[0] = images.nextS;
     nextP[1] = images.nextS1;
     nextP[2] = images.nextS2;
@@ -83,8 +86,8 @@ function Stage(images) {
     this.canvas = document.getElementById("myCanvas");
     this.context = this.canvas.getContext("2d");
 
-    this.playing = 0;
 
+    this.playing = 0;
     this.ResetStage();
 }
 
@@ -92,22 +95,94 @@ Stage.prototype.getNextRandomIndex = function() {
     var a = [1, 2, 3, 4, 5, 6, 7];
     shuffle(a);
     return a[0];
-}
+};
+
+Stage.prototype.LoadKeyConfiguration = function() {
+
+    //All the keyboard interactions mapping, default value
+
+    //Left: J - 74
+    this.TempLeftKey = 74;
+    this.LeftKey = 74;
+    //Right: L - 76
+    this.TempRightKey = 76;
+    this.RightKey = 76;
+    //Down: K - 75
+    this.TempDownKey = 75;
+    this.DownKey = 75;
+    //Hold: O - 79
+    this.TempHoldKey = 79;
+    this.HoldKey = 79;
+    //Drop: U - 85
+    this.TemDropKey = 85;
+    this.DropKey = 85;
+    //Rotate: I - 73
+    this.TempRotateKey = 73;
+    this.RotateKey = 73;
+    //Start game: Space - 32
+    this.StartGameKey = 32;
+    //Pause - P
+    this.PauseKey = 80;
+
+    $('left_key').setProperty("value", "j");
+    $('right_key').setProperty("value","l");
+    $('rotate_key').setProperty("value","i");
+    $('hold_key').setProperty("value","o");
+    $('drop_key').setProperty("value","u");
+    $('down_key').setProperty("value","k");
+};
+
+
+Stage.prototype.SaveKey = function(callback) {
+
+    this.LeftKey = this.TempLeftKey;
+    this.RightKey = this.TempRightKey;
+    this.DownKey = this.TempDownKey;
+    this.HoldKey = this.TempHoldKey;
+    this.DropKey = this.TemDropKey;
+    this.RotateKey = this.TempRotateKey;
+
+    callback();
+};
+
+Stage.prototype.Pause = function (){
+
+    if (this.paused == 0) {
+        this.paused = 1;
+        this.playing = 0;
+        this.DrawMatrix(1);
+        this.context.font = "38pt Calibri Black";
+        this.context.fillStyle = 'black';
+        this.context.fillText("Game", 18, 100);
+        this.context.fillText("Paused", 8, 150);
+        this.context.font = "15pt Calibri";
+        this.context.fillText("Score: " + this.punteggio, 15, 190);
+        this.context.fillText("Row:" + this.righe, 15, 220);
+        this.context.fillText("Level:" + this.livello, 15, 250);
+        this.context.font = "10pt Calibri";
+        this.context.fillText("Press \"P\" to restart the game", 5, 295);
+    } else {
+        this.OnTick();
+        this.paused = 0;
+        this.playing = 1;
+    }
+
+};
 
 Stage.prototype.getNextPezzoIndex = function () {
     index = this.nextPezzi.shift();
     this.nextPezzi.push(this.getNextRandomIndex());
     return index;
-}
+};
 
 Stage.prototype.ResetStage = function () {
     this.context.font = "40pt Calibri";
     this.context.fillText("Space", 15, 100);
     this.context.fillText("to", 55, 150);
     this.context.fillText("Start", 30, 200);
-}
+};
 
-//disegno la matrice nel canvas
+//Draw matrin inside the canvas
 Stage.prototype.DrawMatrix = function (clear) {
     for (i = 0; i < 10; i ++) {                        
             for (ii = 0; ii < 20; ii ++) {
@@ -117,20 +192,20 @@ Stage.prototype.DrawMatrix = function (clear) {
             }                        
     }
      
-}
+};
 
 Stage.prototype.DrawHold = function () {
-        if (this.hold != 0) {
+    if (this.hold != 0) {
         matrixPixel = this.next[this.hold][1];
-        this.context.drawImage(matrixPixel, 162, 8, 30, 36);        
+        this.context.drawImage(matrixPixel, 162, 8, 30, 36);
     } else {
         this.context.fillStyle = "#FFFFFF";
-        this.context.fillRect (162,8,30,36);
+        this.context.fillRect(162, 8, 30, 36);
     }
-}
+};
 
-//disegno i prossimo pezzi
-Stage.prototype.DrawNexts = function (clear) {
+//Draw next pieces
+Stage.prototype.DrawNexts = function () {
 
     offsetX = 160;
     offsetY = 53;
@@ -146,7 +221,7 @@ Stage.prototype.DrawNexts = function (clear) {
         this.context.drawImage(matrixPixel, offsetX + 65, offsetY + 55 +((i-2)*31), 25, 30);    
     }
 
-}
+};
 
 //collisioni orizzontali
 Stage.prototype.CheckHCollision = function(dir) {
@@ -177,9 +252,9 @@ Stage.prototype.CheckHCollision = function(dir) {
 
     return check;
 
-}
+};
 
-//collisioni verticali
+//vertical collision
 Stage.prototype.CheckVCollision = function(dir) {
 
     var check = false;
@@ -203,21 +278,21 @@ Stage.prototype.CheckVCollision = function(dir) {
 
     return check;
 
-}
+};
 
-//collisioni rotatorie
-Stage.prototype.CheckRotoCollision = function() {
+//rolling collision
+Stage.prototype.CheckRollingCollision = function() {
     var check = false;
     if (this.CheckHCollision(0) && this.CheckVCollision(0))
         check = true;
 
     return check;
-}
+};
 
 //fisso il pezzo alla matrice
 Stage.prototype.FixPezzo = function() {
     delete this.Pezzo;
-    this.Pezzo = new Pezzo(this.getNextPezzoIndex()); 
+    this.Pezzo = new Piece(this.getNextPezzoIndex());
     this.CheckRigheFatte();
     this.switched = 0;
     for (i = 0; i < 10; i ++) {
@@ -228,7 +303,7 @@ Stage.prototype.FixPezzo = function() {
     this.DrawPezzo();
     this.DrawNexts();
     this.DrawScore();
-}
+};
 
 /**
  * Disegno il riassunto dei punteggi
@@ -245,7 +320,7 @@ Stage.prototype.DrawScore = function() {
     this.context.fillText(this.righe, 200, 264);
     this.context.fillText(this.righeToNextLevel, 200, 284);
     this.context.fillText(this.livello, 200, 304);
-}
+};
 
 //muovo oriz
 Stage.prototype.MoveX = function(x) {
@@ -256,9 +331,9 @@ Stage.prototype.MoveX = function(x) {
     }
     this.DrawPezzo();
     this.OnTick();                
-}
+};
 
-//muovo vert
+//vertical movement
 Stage.prototype.MoveY = function(y) {
 
     this.DrawPezzo(1);
@@ -271,9 +346,9 @@ Stage.prototype.MoveY = function(y) {
     }
 
     this.OnTick();                
-}
+};
 
-//droppo
+//Drop the floating piece
 Stage.prototype.Drop = function() {
 
     righe = 0;
@@ -287,7 +362,7 @@ Stage.prototype.Drop = function() {
     this.FixPezzo();
     
     this.OnTick();                
-}
+};
 
 //ruoto il pezzo, carico il prossimo frame
 Stage.prototype.Rotate = function() {
@@ -295,23 +370,25 @@ Stage.prototype.Rotate = function() {
     this.DrawPezzo(1);
     backupFrame = this.Pezzo.frameSelezionato;
     this.Pezzo.nextFrame();
-    if (!this.CheckRotoCollision()) {                        
+    if (!this.CheckRollingCollision()) {
         this.Pezzo.frameSelezionato = backupFrame;
     }
     this.DrawPezzo();
     this.OnTick();                
-}
+};
 
 Stage.prototype.MoveDown = function () {
     if (this.playing == 1)
         this.MoveY(1);
-}
-Stage.prototype.MoveSx = function () {
+};
+
+Stage.prototype.MoveLeft = function () {
     this.MoveX(-1);
-}
-Stage.prototype.MoveDx = function () {
+};
+
+Stage.prototype.MoveRight = function () {
     this.MoveX(1);
-}
+};
 
 Stage.prototype.Hold = function () {
     
@@ -320,25 +397,25 @@ Stage.prototype.Hold = function () {
         this.DrawPezzo(true);
         delete this.Pezzo;
         if (this.hold != 0) {
-            this.Pezzo = new Pezzo(this.hold);
+            this.Pezzo = new Piece(this.hold);
         } else {            
-            this.Pezzo = new Pezzo(this.getNextPezzoIndex());
+            this.Pezzo = new Piece(this.getNextPezzoIndex());
             this.DrawNexts();
         }
         this.switched = 1;
         this.hold = tempIndex;
         this.DrawHold();
         this.DrawPezzo();
+        this.MoveDown();
     }
 
-}
-
+};
 
 Stage.prototype.resetRow = function(r) {
         for (i = 0; i < 10; i ++) {
                 this.matrix[r][i] = 0;
         }
-}
+};
 
 Stage.prototype.deleteMatrixRow = function (r){
 
@@ -351,7 +428,8 @@ Stage.prototype.deleteMatrixRow = function (r){
 
     this.resetRow(0);
 
-}            
+};
+
 //controllo se ci sono delle righe
 Stage.prototype.CheckRigheFatte = function() {
 
@@ -396,7 +474,7 @@ Stage.prototype.CheckRigheFatte = function() {
     }
 
 
-}
+};
 
 //metto il pezzo dentro la matrice
 Stage.prototype.DrawPezzo = function (clear) {
@@ -419,37 +497,44 @@ Stage.prototype.DrawPezzo = function (clear) {
 //ad ogni tick esegui questo
 Stage.prototype.OnTick = function () {
 
-    if (this.playing == 1) {        
-        this.DrawMatrix();
-    } else if (this.playing == 2) {
-        this.DrawMatrix(1);
-        this.context.font = "40pt Calibri";
-        this.context.fillText("Game", 18, 100);
-        this.context.fillText("Over", 30, 150);
-        this.context.font = "15pt Calibri";
-        this.context.fillText("Score: " + this.punteggio, 15, 190);
-        this.context.fillText("Row:" + this.righe, 15, 220);
-        this.context.fillText("Level:" + this.livello, 15, 250);
-        this.context.font = "10pt Calibri";
-        this.context.fillText("Space to start a new game", 8, 295);
+    if (this.paused == 0) {
+
+        if (this.playing == 1) {
+            this.DrawMatrix();
+        } else if (this.playing == 2) {
+            this.DrawMatrix(1);
+            this.context.font = "40pt Calibri";
+            this.context.fillText("Game", 18, 100);
+            this.context.fillText("Over", 30, 150);
+            this.context.font = "15pt Calibri";
+            this.context.fillText("Score: " + this.punteggio, 15, 190);
+            this.context.fillText("Row:" + this.righe, 15, 220);
+            this.context.fillText("Level:" + this.livello, 15, 250);
+            this.context.font = "10pt Calibri";
+            this.context.fillText("Space to start a new game", 8, 295);
+        }
     }
-}
+};
 
 Stage.prototype.OnTimer = function() {
-    this.MoveDown();
+
+    if (this.paused == 0) {
+        this.MoveDown();
+    }
+
     var Time = 1050 - (this.livello * 100);
     setTimeout ( "stage.OnTimer()",  Time);
-}
+};
 
-//inizio a giocare!
+//Let's start playing
 Stage.prototype.StartGame = function(){
 
     if (this.playing != 1) {                    
 
-        //resetto la matrix
-        this.matrix = new Array();
+        //reset the matrix
+        this.matrix = [];
         for (i = 0; i < 10; i ++) {
-                mRow = new Array();
+                mRow = [];
                 for (ii = 0; ii < 20; ii ++) {
                         mRow[ii] = 0;                                
                 }
@@ -465,7 +550,7 @@ Stage.prototype.StartGame = function(){
         this.punteggio = 0;
         this.righe = 0;
         this.livello = 1;
-        this.Pezzo = new Pezzo(this.getNextPezzoIndex());   
+        this.Pezzo = new Piece(this.getNextPezzoIndex());
         this.DrawPezzo();
         this.DrawNexts();
         this.DrawScore();
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 /*
-   @author Marco Baldini
-   @license GNU General Public License, version 2 (GPLv2)
+    @author Kaiserbaldo - kaiserbaldo@gmail.com
+    @license GNU General Public License, version 2 (GPLv2)
  */
 
 
@@ -8,13 +8,14 @@ function loadImages(sources, callback){
                 var images = {};
                 var loadedImages = 0;
                 var numImages = 0;
-                
+                var src;
+
                 // get num of sources
-                for (var src in sources) {
+                for (src in sources) {
                     numImages++;
                 }
                 
-                for (var src in sources) {
+                for (src in sources) {
                     images[src] = new Image();
                     images[src].onload = function(){
                         if (++loadedImages >= numImages) {
@@ -32,30 +33,190 @@ shuffle = function(v){
 
 function keyPressed(e) {
 
-    var unicode=e.keyCode? e.keyCode : e.charCode;
-    
-    switch (unicode) {
-        case 32://space: 32 
-            stage.StartGame();
-        break;
-        case 38://freccia su: 38 
-            stage.Rotate();
-        break;
-        case 40://freccia giu: 40
-            stage.MoveDown();
-        break;
-        case 37: //freccia sx: 37
-            stage.MoveSx();
-        break; 
-        case 39://freccia dx: 39
-            stage.MoveDx();
-        break;
-        case 17: //crtl : 17
-            stage.Hold();
-        break;
-        case 96://0 ins : 96
-            stage.Drop();
-        break;                    
+    var unicode=e.code;
+    //alert(unicode);
+
+    if ((unicode == stage.StartGameKey)&&(stage.playing !=1)) {
+        stage.StartGame();
+    } else if ((unicode == stage.RotateKey)&&(stage.playing ==1)) {
+        stage.Rotate();
+    } else if ((unicode == stage.DownKey)&&(stage.playing ==1)) {
+        stage.MoveDown();
+    } else if ((unicode == stage.LeftKey)&&(stage.playing ==1)) {
+        stage.MoveLeft();
+    } else if ((unicode == stage.RightKey)&&(stage.playing ==1)) {
+        stage.MoveRight();
+    } else if ((unicode == stage.HoldKey)&&(stage.playing ==1)) {
+        stage.Hold();
+    } else if ((unicode == stage.DropKey)&&(stage.playing ==1)) {
+        stage.Drop();
+    } else if ((unicode == stage.PauseKey)&&( stage.playing ==1 || stage.paused==1 )) {
+        stage.Pause();
     }
+
+}
+
+function saveKey() {
+
+    stage.SaveKey(function(){
+        alert("Configuration Saved!");
+    });
+}
+
+function preSaveKey(e) {
+
+    var unicode=e.code;
+    //$(this).value
+
+    switch ($(this).id) {
+
+        case 'left_key':
+            stage.TempLeftKey = unicode;
+        break;
+        case 'rotate_key':
+            stage.TempRotateKey = unicode;
+            break;
+        case 'right_key':
+            stage.TempRightKey = unicode;
+        break;
+        case 'hold_key':
+            stage.TempHoldKey = unicode;
+        break;
+        case 'drop_key':
+            stage.TemDropKey = unicode;
+        break;
+        case 'down_key':
+            stage.TempDownKey = unicode;
+        break;
+    }
+
+
+}
+
+function setupDom() {
+
+    if (typeof(Storage) === "undefined") {
+        alert("No web storage support, please use a proper browser");
+    } else {
+
+        var canvas = document.getElementById("myCanvas");
+        var context = canvas.getContext("2d");
+
+
+        var sources = {
+            azzurro:"assets/azzurro.bmp",
+            blu:"assets/blu.bmp",
+            giallo:"assets/giallo.bmp",
+            blu:"assets/blu.bmp",
+            nero:"assets/nero.bmp",
+            rosso:"assets/rosso.bmp",
+            nero:"assets/nero.bmp",
+            arancione:"assets/arancione.bmp",
+            viola:"assets/viola.bmp",
+            verde:"assets/verde.bmp",
+            trasp:"assets/trasp.bmp",
+            white:"assets/white.bmp",
+
+            nextI:"assets/i.bmp",
+            nextI1:"assets/i1.bmp",
+            nextI2:"assets/i2.bmp",
+
+            nextL:"assets/l.bmp",
+            nextL1:"assets/l1.bmp",
+            nextL2:"assets/l2.bmp",
+
+            nextLO:"assets/lo.bmp",
+            nextLO1:"assets/lo1.bmp",
+            nextLO2:"assets/lo2.bmp",
+
+            nextO:"assets/o.bmp",
+            nextO1:"assets/o1.bmp",
+            nextO2:"assets/o2.bmp",
+
+            nextS:"assets/s.bmp",
+            nextS1:"assets/s1.bmp",
+            nextS2:"assets/s2.bmp",
+
+            nextT:"assets/t.bmp",
+            nextT1:"assets/t1.bmp",
+            nextT2:"assets/t2.bmp",
+
+            nextZ:"assets/z.bmp",
+            nextZ1:"assets/z1.bmp",
+            nextZ2:"assets/z2.bmp"
+        };
+
+        var borderColor = "#ff0000";
+        var borderWidth = 10;
+        var xOffset = 5;
+
+
+        context.lineWidth = borderWidth;
+        context.strokeStyle = borderColor;
+        //Bordo sinistro
+        context.moveTo(0 + xOffset, 0);
+        context.lineTo(0 + xOffset, 305);
+        context.stroke();
+
+        //Bordo basso
+        context.moveTo(0 + xOffset, 300);
+        context.lineTo(150 + xOffset, 300);
+        context.stroke();
+
+        //Bordo destro
+        context.moveTo(150 + xOffset, 305);
+        context.lineTo(150 + xOffset, 0);
+        context.stroke();
+
+        //Casella hold
+        context.lineWidth = 6;
+        context.strokeStyle = borderColor;
+        context.moveTo(156, 50);
+        context.lineTo(199, 50);
+        context.stroke();
+        context.moveTo(196, 50);
+        context.lineTo(196, 0);
+        context.stroke();
+        context.moveTo(196, 3);
+        context.lineTo(156, 3);
+        context.stroke();
+        context.font = "12pt Calibri";
+        context.fillText("Hold", 213, 30);
+
+        //Casella next
+        context.lineWidth = 6;
+        context.strokeStyle = borderColor;
+        context.moveTo(160, 50);
+        context.lineTo(215, 50);
+        context.stroke();
+        context.moveTo(213, 47);
+        context.lineTo(213, 113);
+        context.stroke();
+        context.moveTo(216, 116);
+        context.lineTo(160, 116);
+        context.stroke();
+        context.font = "12pt Calibri";
+        context.fillText("Next", 220, 57);
+
+        context.font = "10pt Calibri";
+        context.fillText("Score:", 165, 244);
+        context.fillText("Lines:", 165, 264);
+        context.fillText("LtNL:", 165, 284);
+        context.fillText("Level:", 165, 304);
+
+
+        loadImages(sources, function (assets) {
+            stage = new Stage(assets);
+            document.getElementById("preloader").style.display = "none";
+            document.getElementById("myCanvas").style.display = "block";
+            $('body').addEvent('keydown', keyPressed);
+            $('submit').addEvent('click', saveKey);
+            $$('.key_selector').addEvent('keyup', preSaveKey);
+        });
+
+
+    }
+
+
 
 }
